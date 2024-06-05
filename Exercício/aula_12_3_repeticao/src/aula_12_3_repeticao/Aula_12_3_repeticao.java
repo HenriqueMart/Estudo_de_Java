3a689648fc87637776640e8cc251b0f8bd4a7c45
HenriqueMart <h.martins.silva2014@gmail.com>
2024-06-05 18:11:17 -0300

    Aula e Exercício_12_3_Laço_de_repetição

---
+++ Exercício/aula_12_3_repeticao/src/aula_12_3_repeticao/Aula_12_3_repeticao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula_12_3_repeticao;
+
+/**
+ *
+ * @author hmart
+ */
+public class Aula_12_3_repeticao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc = 0; cc <100; cc += 10){
+           System.out.println("Teste " + cc); 
+        }
+    }
+        
+}
