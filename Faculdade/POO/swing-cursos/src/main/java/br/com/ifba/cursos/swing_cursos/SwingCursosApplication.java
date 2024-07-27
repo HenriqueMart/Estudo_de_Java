b1e180d3c533e58c9c770fdfcadf3cc65481694d
HenriqueMart <h.martins.silva2014@gmail.com>
2024-07-27 10:53:32 -0300

    uptade

--- Faculdade/POO/swing-cursos/src/main/java/br/com/ifba/cursos/swing_cursos/SwingCursosApplication.java
+++ Faculdade/POO/swing-cursos/src/main/java/br/com/ifba/cursos/swing_cursos/SwingCursosApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SwingCursosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SwingCursosApplication.class, args);
+		
+            ConfigurableapplicationContext context = new 
 	}
 
 }
