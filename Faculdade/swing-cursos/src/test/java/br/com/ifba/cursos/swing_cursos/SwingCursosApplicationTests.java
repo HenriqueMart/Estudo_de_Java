6e3510e8bbdbc883346a67a1464ebc153be408da
HenriqueMart <h.martins.silva2014@gmail.com>
2024-07-27 11:02:22 -0300

    Reestruturando as pastas

---
+++ Faculdade/swing-cursos/src/test/java/br/com/ifba/cursos/swing_cursos/SwingCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.cursos.swing_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
