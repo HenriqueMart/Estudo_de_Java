5786b9ec92563ce31ba85988376c83fd89c33efd
HenriqueMart <h.martins.silva2014@gmail.com>
2024-08-03 15:57:09 -0300

    New class ArrayList

--- Estudos/AULAS/Utilizando-arrayListAula-Extra 03/ArrayList/src/App.java
+++ Estudos/AULAS/Utilizando-arrayListAula-Extra 03/ArrayList/src/App.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+       
+        ArrayList<Integer> numeros = new ArrayList<>();
+        ArrayList<String> carros = new ArrayList<>();
+
+        numeros.add(11);
+        numeros.add(12);
+        numeros.add(45);
+
+        carros.add("UNO");
+        carros.add("SUV");
+        carros.add("Polo");
+        carros.add(0, "Argo");
+
+        System.out.println(carros.get(0));
+
+
+        /*for(int e: numeros){
+            System.out.println(e);
+        }
+
+        for (String e:carros) {
+            System.out.println(e);
+        }*/
+
     }
 }
